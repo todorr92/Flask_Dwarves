49f41d9e410c70e1daac6bda36436abc5400a279
todorr92 <todorovicslobodan10@gmail.com>
2020-08-05 20:30:01 +0000

    added contact.html and base.html

--- flask/run.py
+++ flask/run.py
@@ -14,6 +14,11 @@ def about():
     return render_template("about.html")
 
 
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
